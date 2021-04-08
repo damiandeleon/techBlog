15c8e32c4abf97bc9a7de783cd47b9a68ac0bab9
damian.deleon <damianmdeleon@gmail.com>
2021-04-08 14:44:17 -0500

    debug homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Comment, User, Post } = require('../models');
-const withAuth = require('../utils/auth');
+const sequelize = require ('../config/connection');
 
 
 router.get('/', async (req, res) => {
