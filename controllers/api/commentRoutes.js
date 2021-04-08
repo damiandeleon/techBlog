3af704c1b11f7852c7813e949617612419b84c15
damian.deleon <damianmdeleon@gmail.com>
2021-04-08 12:53:28 -0500

    updated seed data

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,29 +1,6 @@
 const router = require('express').Router();
-const { Comment } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
-
-router.get('/', (req, res) => {
-  try {
-    const commentData = await Comment.findAll({})
-
-    res.status(200).json(commentData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-router.get('/:id', (req, res) => {
-  try {
-    const commentData = await Comment.findAll({
-      where: {
-        id: req.params.id
-      }
-    })
-    res.status(200).json(commentData);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
 
 router.post('/', withAuth, async (req, res) => {
   try {
