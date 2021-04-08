949f891cc0b0a304f825cf199cdcdf82b44e61ac
damian.deleon <damianmdeleon@gmail.com>
2021-04-08 13:31:43 -0500

    updated postRoutes

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,6 +1,81 @@
 const router = require('express').Router();
-const { Comment } = require('../../models');
+const { Comment, Post, User } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+//pulling up all posts made (usually upon getting to the site)
+
+router.get('/', (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      attributes: [
+        'id',
+        'title',
+        'content',
+        'created_at'
+      ],
+      include: [{
+        model: User,
+        attributes: ['username']
+      },
+      {
+        model: Comment,
+        attributes: [
+          'id',
+          'text',
+          'post_id',
+          'user_id',
+          'created_at'
+        ],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      }],
+    });
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+//When the user is trying to find a post by id
+
+router.get('/:id', (req, res) => {
+  try {
+    const postData = await Post.findOne({
+      where: {
+        id: res.params.id
+      },
+      attributes: [
+        'id',
+        'content',
+        'title',
+        'created_at'
+      ],
+      include: [{
+        model: User,
+        attributes: ['username']
+      },
+      {
+        model: Comment,
+        attributes: [
+          'id',
+          'text',
+          'post_id',
+          'user_id',
+          'created at'
+        ],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      }]
+    });
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+})
 
 router.post('/', withAuth, async (req, res) => {
   try {
